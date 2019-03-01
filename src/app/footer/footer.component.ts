bce6546c0baead8a9fb32cf3889162b17f73d6b6
Acilio Silva <eng.acilio@gmail.com>
2019-03-01 15:26:25 +0000

    Footer Update Code

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  year = Date.now();
+  links = [
+    {
+      name:'About us',
+      path:'about',
+      type:'sepro'
+    },
+    {
+      name:'Help',
+      path:'help',
+      type:'sepro'
+    },
+    {
+      name:'Privacy Ppolicy',
+      path:'privacy',
+      type:'sepro'
+    },
+    {
+      name:'Terms of Use',
+      path:'terms',
+      type:'sepro'
+    },
+    {
+      name:'Profile',
+      path:'profile',
+      type:'account'
+    },
+    {
+      name:'Services',
+      path:'services',
+      type:'account'
+    },
+    {
+      name:'Tickets',
+      path:'tickets',
+      type:'account'
+    },
+    {
+      name:'Favorite',
+      path:'favorite',
+      type:'account'
+    }
+  ];
+  year = new Date().getFullYear();
+  
   constructor() { }
 
   ngOnInit() {
