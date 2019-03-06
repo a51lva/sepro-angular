7644a4996e60e28c8b439196faabda456605fc31
Acilio Silva <eng.acilio@gmail.com>
2019-03-06 17:58:33 +0000

    Login Component updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,28 +10,52 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  username: FormControl;
+  password: FormControl;
+  formValidation:boolean = false;
+  formValidationMessage:string = "Please validate your form and try again!";
+
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     if(this.authService.isAuthenticaded()){
       this.router.navigateByUrl('/home');
     }
-    
-    this.loginForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
-    });
+
+    this.createFormControls();
+    this.createForm();
   }
 
   onSubmit(){
     let username = this.loginForm.value.username;
     let password = this.loginForm.value.password;
-    
-    this.authService.authenticate(username,password).subscribe((res) => {
-      let token = res['authorization']
-      this.authService.setToken(token)
-      this.router.navigateByUrl('/home');
-    })
+    this.formValidation = this.loginForm.invalid;
+
+    if(this.loginForm.valid){
+      this.authService.authenticate(username,password).subscribe((res) => {
+        let token = res['authorization'];
+        if(token){
+          this.authService.setToken(token)
+          this.router.navigateByUrl('/home');
+        }else{
+          this.password.setValue('');
+          this.formValidation = true;
+          this.formValidationMessage = res['message'];
+        }
+      })
+    }
+  }
+
+  createFormControls(){
+    this.username = new FormControl('', Validators.required);
+    this.password = new FormControl('', Validators.required);
+  }
+
+  createForm(){
+    this.loginForm = new FormGroup({
+      username: this.username,
+      password: this.password
+    });
   }
 
 }
