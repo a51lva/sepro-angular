f06997dd01c6bd6eb957aca77e8488ced6f6f47c
Acilio Silva <eng.acilio@gmail.com>
2019-03-13 11:02:00 +0000

    Create Offer component update

--- src/app/offer/create-offer/create-offer.component.ts
+++ src/app/offer/create-offer/create-offer.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { OfferService } from 'src/app/offer.service';
-import { Observable } from 'rxjs';
-import { Offer } from 'src/app/offer';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -15,11 +13,12 @@ export class CreateOfferComponent implements OnInit {
   description: FormControl;
   location: FormControl;
   reward: FormControl;
-  category: FormControl;
+  service_category: FormControl;
   priority: FormControl;
-  startDate: FormControl;
-  endDate: FormControl;
-
+  start_date: FormControl;
+  end_date: FormControl;
+  formValidation:boolean = false;
+  formValidationMessage:string = "Please validate your form and try again!";
 
   constructor(private offerService: OfferService) { }
 
@@ -28,8 +27,32 @@ export class CreateOfferComponent implements OnInit {
     this.createForm();
   }
 
-  onSubmit(offer:Offer):Observable<Offer>{
-    return this.offerService.create(offer);
+  onSubmit(){
+    this.formValidation = !this.createOfferForm.valid;
+    const item = this.createOfferForm.value;
+    
+    if(this.createOfferForm.valid){
+      this.offerService.create(item).subscribe(
+        (result) => {
+          console.log(JSON.stringify(result))
+        },
+        (error) => {
+          this.formValidation = true;
+          this.formValidationMessage = "Error on creating the offer, please validate your form and submit again!";
+        },
+        () => {
+          this.createOfferForm.reset();
+          alert("Request complete sucessfully!");
+          console.log("Request Complete")}
+      )
+    }else{
+      if(this.createOfferForm.errors !== null){
+        this.formValidationMessage = this.createOfferForm.errors.dates;
+      }else{
+        this.formValidationMessage = "Please validate your form and submit again!";
+      }
+      
+    }
   }
 
   createFormControls(){
@@ -37,10 +60,10 @@ export class CreateOfferComponent implements OnInit {
     this.description = new FormControl('', Validators.required);
     this.location = new FormControl('', Validators.required);
     this.reward = new FormControl('', Validators.required);
-    this.category = new FormControl('', Validators.required);
+    this.service_category = new FormControl('', Validators.required);
     this.priority = new FormControl('', Validators.required);
-    this.startDate = new FormControl('', Validators.required);
-    this.endDate = new FormControl('', Validators.required);
+    this.start_date = new FormControl('', Validators.required);
+    this.end_date = new FormControl('', Validators.required);
   }
 
   createForm(){
@@ -49,11 +72,24 @@ export class CreateOfferComponent implements OnInit {
       description: this.description,
       location: this.location,
       reward: this.reward,
-      category: this.category,
+      service_category: this.service_category,
       priority: this.priority,
-      startDate: this.startDate,
-      endDate: this.endDate
-    });
+      start_date: this.start_date,
+      end_date: this.end_date
+    }, {validators: this.dateLessThan('start_date', 'end_date')});
+  }
+
+  dateLessThan(from: string, to: string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      let f = group.controls[from];
+      let t = group.controls[to];
+      if (f.value > t.value) {
+        return {
+          dates: "Start Date should be less than End Date"
+        };
+      }
+      return {};
+    }
   }
 
 }
